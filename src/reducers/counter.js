e3279a4607b85adf5a89e814250f0b68a08ad79a
Max Malm <benjick@dumfan.net>
2015-12-23 21:51:46 +0100

    Add Immutable.js to state

--- src/reducers/counter.js
+++ src/reducers/counter.js
@@ -1,11 +1,16 @@
 import { INCREMENT_COUNTER, DECREMENT_COUNTER } from '../actions/counter';
+import { Record } from 'immutable';
 
-export default function counter(state = 0, action) {
+const initialState = Record({
+  value: 0,
+});
+
+export default function counter(state = new initialState, action) {
   switch (action.type) {
     case INCREMENT_COUNTER:
-      return state + 1;
+      return state.set('value', state.value + 1);
     case DECREMENT_COUNTER:
-      return state - 1;
+      return state.set('value', state.value - 1);
     default:
       return state;
   }
