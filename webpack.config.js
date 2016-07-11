474eeae0d13fb183b5f39a41f225af7ecf5f38d7
Max Malm <benjick@dumfan.net>
2016-07-11 17:46:03 +0200

    Change how webpack config works

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,9 @@ const merge = require('webpack-merge');
 const precss = require('precss');
 const autoprefixer = require('autoprefixer');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const validate = require('webpack-validator');
 
+let config;
 const common = {
   entry: [
     './src/css/main.css',
@@ -40,47 +42,49 @@ const common = {
   postcss: () => [precss, autoprefixer],
 };
 
-if (process.env.NODE_ENV === 'production') {
-  module.exports = merge(common, {
-    devtool: 'cheap-module-source-map',
-    plugins: [
-      new webpack.DefinePlugin({
-        'process.env': {
-          NODE_ENV: JSON.stringify('production'),
+switch (process.env.npm_lifecycle_event) {
+  case 'build':
+    config = merge(common, {
+      devtool: 'cheap-module-source-map',
+      plugins: [
+        new webpack.DefinePlugin({
+          'process.env': {
+            NODE_ENV: JSON.stringify('production'),
+          },
+          __DEVELOPMENT__: false,
+        }),
+        new webpack.optimize.OccurenceOrderPlugin(),
+        new webpack.optimize.DedupePlugin(),
+        new webpack.optimize.UglifyJsPlugin({
+          compress: {
+            warnings: false,
+          },
+        }),
+      ],
+    });
+    break;
+  default:
+    config = merge(common, {
+      entry: [
+        ...common.entry,
+        'webpack-dev-server/client?http://localhost:8080',
+        'webpack/hot/dev-server',
+      ],
+      debug: true,
+      devServer: {
+        contentBase: './src',
+        stats: 'errors-only',
+        inline: true,
+        proxy: {
+          '/api/*': 'http://localhost:8080',
         },
-        __DEVELOPMENT__: false,
-      }),
-      new webpack.optimize.OccurenceOrderPlugin(),
-      new webpack.optimize.DedupePlugin(),
-      new webpack.optimize.UglifyJsPlugin({
-        compress: {
-          warnings: false,
-        },
-      }),
-    ],
-  });
-} else {
-  // Development
-  module.exports = merge(common, {
-    entry: [
-      ...common.entry,
-      'webpack-dev-server/client?http://localhost:8080',
-      'webpack/hot/dev-server',
-    ],
-    debug: true,
-    devServer: {
-      contentBase: './src',
-      stats: 'errors-only',
-      inline: true,
-      progress: true,
-      proxy: {
-        '/api/*': 'http://localhost:8080',
       },
-    },
-    plugins: [
-      new webpack.DefinePlugin({
-        'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development'),
-      }),
-    ],
-  });
+      plugins: [
+        new webpack.DefinePlugin({
+          'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development'),
+        }),
+      ],
+    });
 }
+
+module.exports = validate(config);
